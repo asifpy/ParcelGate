93d3d0b108c8161cc24d896c6ead22404bfc3e7a
Asif <saluasif@gmail.com>
2024-04-17 09:31:40 +0300

    added experimental microservice architecture to gitignore

--- microservice/broker_service/broker/core/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Broker
-
-class BrokerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Broker
-        fields = '__all__'
--- microservice/broker_service/broker/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BrokerListCreateAPIView, BrokerRetrieveUpdateDestroyAPIView
-
-app_name = 'core'
-
-urlpatterns = [
-    path('brokers/', BrokerListCreateAPIView.as_view(), name='broker-list'),
-    path('brokers/<int:pk>/', BrokerRetrieveUpdateDestroyAPIView.as_view(), name='broker-detail')
-]
